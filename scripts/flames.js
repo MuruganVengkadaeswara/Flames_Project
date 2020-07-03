315bc5fae47f0290e123f6c94fff8acbe18615aa
Murugan Vengkadaeswara <muruganvengkadaeswara@gmail.com>
2020-07-03 20:48:24 +0530

    Commit

--- scripts/flames.js
+++ scripts/flames.js
@@ -14,51 +14,51 @@ document.getElementById("you").focus();
 matching character and gives the total length*/
 
 function findFlame() {
-  const you = document.getElementById("you").value;
-  const crush = document.getElementById("crush").value;
-  let tot_len = you.length + crush.length;
-  let youArr = you.toLowerCase().split("");
-  let crushArr = crush.toLowerCase().split("");
+    const you = document.getElementById("you").value;
+    const crush = document.getElementById("crush").value;
+    let tot_len = you.length + crush.length;
+    let youArr = you.toLowerCase().split("");
+    let crushArr = crush.toLowerCase().split("");
 
-  load.style.display = "block";
-  btn.disabled = true;
+    load.style.display = "block";
+    btn.disabled = true;
 
-  for (let i = 0; i <= youArr.length; i++) {
-    for (let j = 0; j <= crushArr.length; j++) {
-      if (youArr[i] == crushArr[j]) {
-        crushArr.splice(j, 1);
-        continue;
-      }
+    for (let i = 0; i <= youArr.length; i++) {
+        for (let j = 0; j <= crushArr.length; j++) {
+            if (youArr[i] == crushArr[j]) {
+                crushArr.splice(j, 1);
+                continue;
+            }
+        }
     }
-  }
-  let rem = crush.length - crushArr.length;
-  let tot = Math.abs(tot_len - rem * 2);
-  console.log(tot);
+    let rem = crush.length - crushArr.length;
+    let tot = Math.abs(tot_len - rem * 2);
+    console.log(tot);
 
-  find(tot);
+    find(tot);
 }
 
 /* this function find(num) strikes the characters and calls display() with 
 last character*/
 
 function find(num) {
-  let baseInput = "flames";
-  let temp = "";
+    let baseInput = "flames";
+    let temp = "";
 
-  if (num > 0) {
-    while (baseInput.length !== 1) {
-      // console.log(baseInput); use if needed
-      let tmpLen = num % baseInput.length; //finding character to strike
-      if (tmpLen !== 0) {
-        // appending  start from next char to strike and first char to char before strike
-        temp = baseInput.substring(tmpLen) + baseInput.substring(0, tmpLen - 1);
-      } else {
-        temp = baseInput.substring(0, baseInput.length - 1); // if tmplen = 0 removing last character
-      }
-      baseInput = temp;
+    if (num > 0) {
+        while (baseInput.length !== 1) {
+            // console.log(baseInput); use if needed
+            let tmpLen = num % baseInput.length; //finding character to strike
+            if (tmpLen !== 0) {
+                // appending  start from next char to strike and first char to char before strike
+                temp = baseInput.substring(tmpLen) + baseInput.substring(0, tmpLen - 1);
+            } else {
+                temp = baseInput.substring(0, baseInput.length - 1); // if tmplen = 0 removing last character
+            }
+            baseInput = temp;
+        }
     }
-  }
-  display(baseInput.charAt(0));
+    display(baseInput.charAt(0));
 }
 //============= TRIED WITH ARRAY =========
 // let arr = "flames".split("");
@@ -84,46 +84,46 @@ function find(num) {
 
 function display(char) {
 
-  let str = 'flames';
-  str = str.replace(char, '');
-  console.log(str);
+    let str = 'flames';
+    str = str.replace(char, '');
+    console.log(str);
 
-  for (let i = 0; i < str.length; i++) {
-    setTimeout(() => {
-      document.getElementById(str[i]).style.textDecoration = "line-through";
-      sound.play();
+    for (let i = 0; i < str.length; i++) {
+        setTimeout(() => {
+            document.getElementById(str[i]).style.textDecoration = "line-through";
+            sound.play();
 
-    }, i * 1000);
-  }
-  setTimeout(() => {
-    load.style.display = "none";
-    switch (char) {
-      case 'f':
-        para.textContent = "Friendship";
-        break;
-      case 'l':
-        para.textContent = "Love";
-        break;
-      case 'a':
-        para.textContent = "Affection";
-        break;
-      case 'e':
-        para.textContent = "Enemies";
-        break;
-      case 'm':
-        para.textContent = "Marriage";
-        break;
-      case 's':
-        para.textContent = "Siblings";
-        break;
+        }, i * 1000);
     }
-  }, 6000);
-  setTimeout(() => {
-    reload.style.display = "block";
-  }, 6000);
+    setTimeout(() => {
+        load.style.display = "none";
+        switch (char) {
+            case 'f':
+                para.textContent = "Friendship";
+                break;
+            case 'l':
+                para.textContent = "Love";
+                break;
+            case 'a':
+                para.textContent = "Affection";
+                break;
+            case 'e':
+                para.textContent = "Enemies";
+                break;
+            case 'm':
+                para.textContent = "Marriage";
+                break;
+            case 's':
+                para.textContent = "Siblings";
+                break;
+        }
+    }, 6000);
+    setTimeout(() => {
+        reload.style.display = "block";
+    }, 6000);
 
 }
 
 function reloadPage() {
-  location.reload();
+    location.reload();
 }
--- scripts/snake.js
+++
@@ -1,7 +0,0 @@
-function click() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  ctx.moveTo(0, 0);
-  ctx.lineTo(200, 100);
-  ctx.stroke();
-}
